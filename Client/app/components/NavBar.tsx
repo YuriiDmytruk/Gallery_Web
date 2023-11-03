81bbf3b6cbb911ae18a4673d2a6295245dd1e801
DESKTOP-5015UJ0\Roman <rdmytruk78@gmail.com>
2023-11-03 09:33:27 +0200

    add animation to LogIn

--- Client/app/components/NavBar.tsx
+++ Client/app/components/NavBar.tsx
@@ -1,22 +1,48 @@
 import Link from 'next/link';
 import React from 'react';
 
-const NavBar = () => {
+import '../styles/NavBarAnimation.css';
+
+interface NavBarProps {
+  active: boolean;
+  setActive: (value: boolean) => void;
+}
+
+const NavBar = (props: NavBarProps) => {
+  console.log({...props})
+  
+  const onLogInClick = () => {
+    props.setActive(!props.active);
+  };
+
   return (
-    <div className="navbar bg-accent mx-auto mt-3 max-w-[90%] rounded-lg">
-      <div className="flex-1">
-        <Link href="/galery" className="btn btn-ghost normal-case text-xl">
-          Galery
-        </Link>
-        <div className="ml-10">
-          <Link href="/popular" className="btn btn-ghost normal-case text-sm">
-            Popular Galery
+    <div>
+      <div
+        className={
+          props.active
+            ? 'navbar bg-accent mx-auto rounded-lg mt-3 max-w-[90%] animation'
+            : 'navbar bg-accent mx-auto rounded-lg mt-0 max-w-[100%]'
+        }
+      >
+        <div className="flex-1">
+          <Link href="/galery" className="btn btn-ghost normal-case text-xl">
+            Galery
           </Link>
-          <Link href="/friends" className="btn btn-ghost normal-case text-sm">My Friends</Link>
+          <div className="ml-10">
+            <Link href="/popular" className="btn btn-ghost normal-case text-sm">
+              Popular Galery
+            </Link>
+            <Link href="/friends" className="btn btn-ghost normal-case text-sm">
+              My Friends
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="btn btn-ghost normal-case text-sm">
-        <div>Log in</div>
+        <div
+          className="btn btn-ghost normal-case text-sm"
+          onClick={onLogInClick}
+        >
+          Log in
+        </div>
       </div>
     </div>
   );
