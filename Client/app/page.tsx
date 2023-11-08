4bc3d45e966c67867a81c1671e5c65de611dc7d2
DESKTOP-5015UJ0\Roman <rdmytruk78@gmail.com>
2023-11-08 10:27:45 +0200

    Add style to homePage

--- Client/app/page.tsx
+++ Client/app/page.tsx
@@ -2,7 +2,7 @@ import { getImages } from './util/api';
 
 import ImageCarousel from './components/ImageCarousel';
 
-import {ImageType} from './types'
+import { ImageType } from './types';
 
 const Home = async () => {
   const images = (await getImages('', '20')).value as ImageType[];
@@ -15,7 +15,7 @@ const Home = async () => {
         <ImageCarousel
           time={3000}
           images={images.map((image: any) => image.url)}
-          classNames=''
+          classNames=""
         />
       </div>
 
@@ -23,21 +23,21 @@ const Home = async () => {
         <ImageCarousel
           time={5000}
           images={images.map((image: any) => image.url)}
-          classNames=''
+          classNames=""
         />
       </div>
       <div className="w-[60%] h-[50%] fixed bottom-0 left-0 z-0">
         <ImageCarousel
           time={4000}
           images={images.map((image: any) => image.url)}
-          classNames=''
+          classNames=""
         />
       </div>
       <div className="w-[60%] h-[50%] fixed bottom-0 right-0 z-0">
         <ImageCarousel
           time={3500}
           images={images.map((image: any) => image.url)}
-          classNames=''
+          classNames=""
         />
       </div>
       <div className="w-[100%] h-[100%] fixed flex justify-center items-center z-10">
@@ -45,8 +45,41 @@ const Home = async () => {
           <ImageCarousel
             time={6000}
             images={images.map((image: any) => image.url)}
-            classNames='rounded-full'
+            classNames="rounded-full shadow-2xl shadow-white"
           />
+        </div>
+      </div>
+      <div className="absolute w-full h-full flex justify-around items-center z-20">
+        <div className="card w-[25%] h-[30%] glass">
+          <div className="card-body flex items-start">
+            <div className="card-title text-white w-full flex justify-center font-bold size text-2xl">
+              Creat gallery
+            </div>
+            <div className="card-title text-slate-300 w-full flex h-full items-center text-base">
+              Create your gallery with unique images and expand your worldview
+            </div>
+          </div>
+        </div>
+        <div className="card w-[25%] h-[30%] glass">
+          <div className="card-body">
+            <div className="card-title text-white w-full flex justify-center font-bold size text-2xl">
+              Share with friends
+            </div>
+            <div className="card-title text-slate-300 w-full flex h-full items-center text-base">
+              Share your gallery with friends and discuss your new photos
+            </div>
+          </div>
+        </div>
+        <div className="card w-[25%] h-[30%] glass">
+          <div className="card-body">
+            <div className="card-title text-white w-full flex justify-center font-bold size text-2xl">
+              Rate images
+            </div>
+            <div className="card-title text-slate-300 w-full flex h-full items-center text-base">
+              Rate your friends pictures and wait for your picture to become
+              popular
+            </div>
+          </div>
         </div>
       </div>
     </div>
