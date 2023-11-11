341c554936e4d871c363ffd238371fa231c318b7
DESKTOP-5015UJ0\Roman <rdmytruk78@gmail.com>
2023-11-11 11:49:10 +0200

    Add button to view friends gallery

--- Client/app/components/FriendCard.tsx
+++ Client/app/components/FriendCard.tsx
@@ -1,8 +1,8 @@
 'use client';
 import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 import ImageCarousel from '@/app/components/ImageCarousel';
-import FriendDeleteButton from '@/app/components/FriendDeleteButton';
 
 import { ImageType, UserType } from '@/app/types';
 import { getImages } from '@/app/util/api';
@@ -13,15 +13,25 @@ interface FriendCardProps {
 
 const FriendCard = (props: FriendCardProps) => {
   const [images, setImages] = useState<ImageType[]>([]);
+  const router = useRouter();
 
   useEffect(() => {
     const fetchImages = async () => {
-      const images = (await getImages(props.friend._id, '')).value as ImageType[];
+      const images = (await getImages(props.friend._id, ''))
+        .value as ImageType[];
       setImages(images as ImageType[]);
     };
 
     fetchImages();
   }, [props.friend._id]);
+
+  const onDeleteClick = () => {
+    console.log('Delete friend');
+  };
+
+  const onGaleryClick = () => {
+    router.push(`/gallery/${props.friend._id}`)
+  }
 
   return (
     <div className="card card-compact shadow-xl bg-accent h-72 w-[30%] justify-around mb-5">
@@ -37,7 +47,12 @@ const FriendCard = (props: FriendCardProps) => {
           <h2 className="flex items-center text-xl font-semibold">
             Name: {props.friend.nickName}
           </h2>
-          <FriendDeleteButton />
+          <button className="btn btn-error" onClick={onDeleteClick}>
+            Delete friend
+          </button>
+          <button className="btn btn-success" onClick={onGaleryClick}>
+            Gallery
+          </button>
         </div>
       </div>
     </div>
--- Client/app/pages/GalleryPage.tsx
+++ Client/app/pages/GalleryPage.tsx
@@ -1,8 +1,7 @@
 'use client';
 import React, { useEffect } from 'react';
-import { useRouter  } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
-import PleaseLogIn from '@/app/components/PleaseLogIn';
 import ImageCard from '@/app/components/ImageCard';
 import AddButton from '@/app/components/AddButton';
 
@@ -17,39 +16,39 @@ interface GalleryPageProps {
 
 const GalleryPage = (props: GalleryPageProps) => {
   const dispatch = useDispatch();
-  const router = useRouter()
+  const router = useRouter();
 
-  const userId= useSelector((state: UserState) => state._id);
-  if(userId === ''){
-    router.push('/')
+  const userId = useSelector((state: UserState) => state._id);
+  if (userId === '') {
+    router.push('/');
   }
   const nickName = useSelector((state: UserState) => state.nickName);
   let images = useSelector((state: UserState) => state.images);
-  
+
   useEffect(() => {
     if (images.length === 0) {
       dispatch(setImages(props.images));
     }
   }, [dispatch, images.length, props.images]);
 
-  if(props.userId === 'popular'){
-    images = props.images
+  if (props.userId === 'popular' || props.userId !== userId) {
+    images = props.images;
   }
 
   return (
     <div className="h-full w-full pt-[7%] px-5">
-      {props.userId !== '' ? (
-        <div>
-          <div className="flex flex-wrap justify-around">
-            {images.map((image) => (
-              <ImageCard key={image._id} image={image} page={props.userId} />
-            ))}
-          </div>
-          {props.userId !== 'popular' ? <AddButton userId={userId} userNickName={nickName}/> : <></>}
+      <div>
+        <div className="flex flex-wrap justify-around">
+          {images.map((image) => (
+            <ImageCard key={image._id} image={image} page={props.userId} />
+          ))}
         </div>
-      ) : (
-        <PleaseLogIn />
-      )}
+        {props.userId !== 'popular' ? (
+          <AddButton userId={userId} userNickName={nickName} />
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 };
